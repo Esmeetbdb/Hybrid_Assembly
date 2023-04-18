c9cccb4d3ccaf47d3a296a4fa35b58bf4b8df036
Esmee ten Berk <esmtbdb@DESKTOP-TA433P2.localdomain>
2023-04-18 14:28:44 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+#from distutils.core import setup
+from Cython.Build import cythonize
+
+import numpy
+
+setup(
+    name = 'hybrid assembler',
+    version = '1.1.0'
+
+    url = "https://github.com/Esmeetbdb/Hybrid_Assembly"
+    author = "Esmee ten Berk de Boer"
+
+    ext_modules = cythonize(["process_fasta.py", "find_overlaps_np_parallel.pyx"]),
+    include_dirs=[numpy.get_include()]
+)
