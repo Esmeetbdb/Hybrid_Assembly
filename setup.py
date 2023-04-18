2afb26062a50b984e8eafe2040a91125b35d45a4
Esmeetbdb <92033310+Esmeetbdb@users.noreply.github.com>
2023-04-18 15:38:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ import numpy
 
 setup(
     name = 'hybrid assembler',
-    version = '1.1.0'
+    version = '1.1.0',
 
-    url = "https://github.com/Esmeetbdb/Hybrid_Assembly"
-    author = "Esmee ten Berk de Boer"
+    url = "https://github.com/Esmeetbdb/Hybrid_Assembly",
+    author = "Esmee ten Berk de Boer",
 
     ext_modules = cythonize(["process_fasta.py", "find_overlaps_np_parallel.pyx"]),
     include_dirs=[numpy.get_include()]
